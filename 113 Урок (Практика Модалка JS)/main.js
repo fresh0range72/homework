8d9e8eb57a223845fed47f3da1fb8f4d8ddd419b
Konstantin <freshmeating@mail.ru>
2025-11-30 15:38:59 +0500

    Add files via upload

---
+++ 113 Урок (Практика Модалка JS)/main.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector('.btn-open')
+const modal = document.querySelector('.modal')
+const body = document.body
+
+
+const openModal = () => {
+    modal.classList.add('modal--open')
+    body.classList.add('body--fixed')
+}
+const closeModal = () => {
+    modal.classList.remove('modal--open')
+    body.classList.remove('body--fixed')
+}
+
+
+
+btn.addEventListener('click', openModal)
+
+// modal.addEventListener('click', () => {
+//     modal.classList.remove('modal--open')
+// })
+
+modal.addEventListener('click', event => {
+    const target = event.target 
+
+    if (target && target.classList.contains('modal') || target.classList.contains('modal__close-btn')) {
+        closeModal()
+    }
+})
+
+document.addEventListener('keydown', event => {
+    // console.log(event.code)
+    if (event.code === 'Escape' && modal.classList.contains('modal--open')) {
+        closeModal()
+    }
+})
